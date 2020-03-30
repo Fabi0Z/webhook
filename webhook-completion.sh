640786fd7f9ad81da1935ae8f6deaedff0e7ec69
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-03-30 14:00:34 +0200

    REMOVE returns from completion

--- webhook-completion.sh
+++ webhook-completion.sh
@@ -11,17 +11,15 @@ DEVICES_FILE=$WEBHOOK_FOLDER/devices
 if [ ! -f "$DEVICES_FILE" ]; then # If devices file doesn't exist
     brecho "Devices file doesn't exist!"
     echo "Create the file" $DEVICES_FILE "with the DEVICES variable"
-    return 1
+else
+    source $DEVICES_FILE # LOAD devices
 fi
-
-source $DEVICES_FILE # LOAD devices
 
 if [ -z "$DEVICES" ]; then # If variable it's empty
     brecho "No DEVICES configured in the configuration file"
     echo "Write your DEVICES as a bash variable inside" $DEVICES_FILE
     echo "E.G."
     echo "DEVICES=\"smartDevice smartLamp smartPlug\""
-    return 2
+else
+    complete -W "$DEVICES" webhook # Run command for autocomplete
 fi
-
-complete -W "$DEVICES" webhook # Run command for autocomplete
