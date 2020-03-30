f9b1ad1d501e4999f3c1f40ab5624c20c947b50b
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2020-03-30 14:05:15 +0200

    ADD silent parameter

--- webhook-completion.sh
+++ webhook-completion.sh
@@ -9,17 +9,21 @@ DEVICES_FILE=$WEBHOOK_FOLDER/devices
 # * Script
 
 if [ ! -f "$DEVICES_FILE" ]; then # If devices file doesn't exist
-    brecho "Devices file doesn't exist!"
-    echo "Create the file" $DEVICES_FILE "with the DEVICES variable"
+    if [ "$1" != "-silent" ]; then
+        brecho "Devices file doesn't exist!"
+        echo "Create the file" $DEVICES_FILE "with the DEVICES variable"
+    fi
 else
     source $DEVICES_FILE # LOAD devices
 fi
 
 if [ -z "$DEVICES" ]; then # If variable it's empty
-    brecho "No DEVICES configured in the configuration file"
-    echo "Write your DEVICES as a bash variable inside" $DEVICES_FILE
-    echo "E.G."
-    echo "DEVICES=\"smartDevice smartLamp smartPlug\""
+    if [ "$1" != "-silent" ]; then
+        brecho "No DEVICES configured in the configuration file"
+        echo "Write your DEVICES as a bash variable inside" $DEVICES_FILE
+        echo "E.G."
+        echo "DEVICES=\"smartDevice smartLamp smartPlug\""
+    fi
 else
     complete -W "$DEVICES" webhook # Run command for autocomplete
 fi
